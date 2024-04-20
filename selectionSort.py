8f280eedf568aaa1797cf9b9876e224a8bdaaec0
Prince Kojo Dugboryele <95886564+pryyyynz@users.noreply.github.com>
2024-04-20 13:31:10 +0000

    Selection Sort Algorithm in O(n2)

---
+++ selectionSort.py
@@ -0,0 +1,13 @@
+def selectionSort(array):
+    n = len(array)
+    for i in range(n-1):
+        min_index = i
+        for j in range(i+1, n):
+            if array[j] < array[min_index]:
+                min_index = j
+        min_value = array.pop(min_index)
+        array.insert(i, min_value)
+    print(array)
+nums = [3, 6, 12, 6, 1, 8, 5]
+selectionSort(nums)
+
